1892b4d7422736ec52df1b3d788d3ebdd6e92f0b
Arham Abiyan <arham.abiyan@gmail.com>
2020-08-18 09:48:23 +0700

    refact pg command

---
+++ cmd/pg/ping.go
@@ -0,0 +1,46 @@
+package pg
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+	"github.com/spf13/cobra"
+)
+
+// PingCmd represents the pg command
+var PingCmd = &cobra.Command{
+	Use:   "ping",
+	Short: "Check if PostgreSQL is alive or not",
+	Long:  `Check ping for PostgreSQL.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		address, _ := cmd.Flags().GetString("address")
+		user, _ := cmd.Flags().GetString("user")
+		password, _ := cmd.Flags().GetString("password")
+		db, _ := cmd.Flags().GetString("db")
+
+		pingPgsql(address, user, password, db)
+	},
+}
+
+func init() {
+	PingCmd.Flags().StringP("address", "a", "127.0.0.1:5432", "Host for PostgreSQL")
+	PingCmd.Flags().StringP("user", "u", "postgres", "User for PostgreSQL")
+	PingCmd.Flags().StringP("password", "p", "", "Password for PostgreSQL")
+	PingCmd.Flags().StringP("database", "d", "postgres", "Database for PostgreSQL")
+}
+
+func pingPgsql(address, user, password, db string) {
+	dsn := fmt.Sprintf(`postgres://%s:%s@%s/%s?sslmode=disable`, user, password, address, db)
+
+	conn, err := sql.Open("postgres", dsn)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if err = conn.Ping(); err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Ping sucessful")
+}
